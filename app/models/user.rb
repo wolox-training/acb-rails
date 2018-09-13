f385cf5d53916963690c6d5bbae0d3e69d815e75
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-09-13 17:59:42 -0300

    Auth Implementation4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
-   include DeviseTokenAuth::Concerns::User
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
