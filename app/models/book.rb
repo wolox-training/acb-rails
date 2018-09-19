466906ed5f629afb804c9549d89236a40f7d8090
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-09-19 17:41:21 -0300

    add RSppec,database cleaner,Faker, Factory Bot

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-  validates :gender, :author, :image, :title, :editor, :year, presence: true
+  validates :gender, :author, :image, :title, :publisher, :release_date,
+            presence: true
 end
