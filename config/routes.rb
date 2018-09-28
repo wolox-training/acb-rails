4eb6710ac14896b599c1ee6b9db4b90f8fd0c675
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-09-28 13:59:10 -0300

    add mini_racer gem to fix heroku issue ver3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  api_version(:module => "Api::V1", :path => {:value => "api/v1"}) do
-  resources :books, only: [:index,:show]
+  #api_version(:module => "Api::V1", :path => {:value => "api/v1"}) do
+  #resources :books, only: [:index,:show]
+
+  namespace :api do
+    namespace :v1 do
+        resources :books, only: [:index,:show]
+    end
+  end
+
+
   end
 end
