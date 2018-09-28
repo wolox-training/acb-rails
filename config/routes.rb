4d8ffbeccab81fac8dd98a953ed2d3f20a367da6
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-09-28 14:20:11 -0300

    remove content of rent_rspec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #api_version(:module => "Api::V1", :path => {:value => "api/v1"}) do
-  #resources :books, only: [:index,:show]
-
-  namespace :api do
-    namespace :v1 do
-        resources :books, only: [:index,:show]
-    end
-  end
-
-
+  api_version(:module => "Api::V1", :path => {:value => "api/v1"}) do
+    resources :books, only: [:index,:show]
   end
 end
