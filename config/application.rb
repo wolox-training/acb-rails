d3fb4ccfddbadecab3e74e9860b1840d8b0c5688
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-09-28 10:38:01 -0300

    Failed to push some refs

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+#config.assets.initialize_on_precompile = false
 module AcbRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
