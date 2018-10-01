d33a38c680fd1e6c28793ad0328ad1328181d90a
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-01 09:37:00 -0300

    Initial release of the branch

---
+++ app/controllers/api/v1/rents_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    class rentsController < ApplicationController
+      include Wor::Paginate
+
+      def create
+
+      end
+
+      def index
+
+      end
+      
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   api_version(:module => "Api::V1", :path => {:value => "api/v1"}) do
     resources :books, only: [:index,:show]
+    resources :rents, only: [:create, :index]
   end
 end
