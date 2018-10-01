36c9a8576b803428130651f2a908ebbc62a8138e
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-01 17:58:34 -0300

    correct rubocop offenses

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -4,25 +4,24 @@ module Api
       include Wor::Paginate
 
       def index
-        @rents = Rent.includes(:book, :user).where( {:user_id=>params[:id]} )
+        @rents = Rent.includes(:book, :user).where(user_id: params[:id])
         render_paginated @rents
       end
 
       def create
         rent = Rent.new(rent_params)
-         if rent.save
-           render json: rent
-         else
-           render json: rent.errors, status: :unprocessable_entity
-         end
-       end
+        if rent.save
+          render json: rent
+        else
+          render json: rent.errors, status: :unprocessable_entity
+        end
+      end
 
       private
 
       def rent_params
-       params.permit(:user_id, :book_id, :from, :to)
+        params.permit(:user_id, :book_id, :from, :to)
       end
-
     end
   end
 end
