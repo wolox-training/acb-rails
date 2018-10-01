310edbad2c454c20f5aec6c0f84a8db8e42568e8
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-01 17:52:07 -0300

    rents_controller modification

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -4,24 +4,25 @@ module Api
       include Wor::Paginate
 
       def index
-        @rents = Rent.includes(:book, :user).where(user_id: params[:id])
+        @rents = Rent.includes(:book, :user).where( {:user_id=>params[:id]} )
         render_paginated @rents
       end
 
       def create
-        rent = Rent.create(rent_params)
-        if rent.valid? # rent.save
-          render json: rent
-        else
-          render json: rent.errors, status: :unprocessable_entity
-        end
-      end
+        rent = Rent.new(rent_params)
+         if rent.save
+           render json: rent
+         else
+           render json: rent.errors, status: :unprocessable_entity
+         end
+       end
 
       private
 
       def rent_params
-        params.permit(:user_id, :book_id, :from, :to)
+       params.permit(:user_id, :book_id, :from, :to)
       end
+
     end
   end
 end
