43deb850494eb9848252f4f2ae2e525057dec9aa
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-01 11:21:29 -0300

    rents_controller modif

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,25 +1,26 @@
 module Api
   module V1
-    class rentsController < ApplicationController
+    class RentsController < ApplicationController
       include Wor::Paginate
       def index
         render_paginated current_user.rents
       end
 
       def create
-        book = Book.find(create_params['book_id'])
+        Book.find(create_params['book_id'])
         rent = Rent.new(create_params)
         render json: rent
       end
 
       private
+
       def create_params
         params.permit(:user_id, :book_id, :from, :to)
       end
-     def index_params
-      params.required(:user_id)
-     end
-    end
 
+      def index_params
+        params.required(:user_id)
+      end
+    end
   end
 end
