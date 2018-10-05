34366893a0200782dff28bc27d108712998be138
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-05 16:04:00 -0300

    add I18n modif to aplicaction.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module AcbRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[File.join(::Rails.root, 'config', 'locales', '**', '*.{rb,yml}')]
+    # Use I18n fallbacks
+    config.i18n.fallbacks = true
+    I18n.locale = :es
   end
 end
