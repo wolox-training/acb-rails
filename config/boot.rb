b0136d033a62f8db3a24c52f7dca358e33b6f6f1
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-05 16:50:59 -0300

    modif boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'i18n'
+I18n.default_locale = :en
+I18n.locale = :en
