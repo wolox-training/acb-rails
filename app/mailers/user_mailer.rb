9359ad50e283af5c8bf7c7ca20499f1007ee57c6
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-05 17:01:52 -0300

    modif locale

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(rent)
     @rent = rent
     @book = rent.book
-    I18n.with_locale(rent.user.locale || :es) do
+    I18n.with_locale(rent.user.locale || :en) do
       mail(to: rent.user.email, subject: I18n.t('mails.welcome.subject')) do |format|
         format.html { render 'welcome_email' }
       end
