c8a6f9c98210acd5e2d031935967bad39f767048
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-05 16:37:42 -0300

    tranlation modf

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module AcbRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.load_path += Dir[File.join(::Rails.root, 'config', 'locales', '**', '*.{rb,yml}')]
     # Use I18n fallbacks
     config.i18n.fallbacks = true
     I18n.locale = :en
+    config.action_view.raise_on_missing_translations = false
+        config.i18n.load_path += Dir[File.join(::Rails.root, 'config', 'locales', '**', '*.{rb,yml}')]
   end
 end
