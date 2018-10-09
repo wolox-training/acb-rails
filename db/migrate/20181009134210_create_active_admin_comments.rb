44cb24b4528b15557e8581e331bb28bd9995ab83
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-09 10:50:01 -0300

    db migration

--- db/migrate/20181009134210_create_active_admin_comments.rb
+++ db/migrate/20181009134210_create_active_admin_comments.rb
@@ -1,13 +1,13 @@
 class CreateActiveAdminComments < ActiveRecord::Migration::Current
   def self.up
-    create_table :active_admin_comments do |t|
-      t.string :namespace
-      t.text   :body
-      t.references :resource, polymorphic: true
-      t.references :author, polymorphic: true
-      t.timestamps
+    change_table :active_admin_comments do |t|
+      #t.string :namespace
+      #t.text   :body
+      #t.references :resource, polymorphic: true
+      #t.references :author, polymorphic: true
+      #t.timestamps
     end
-    add_index :active_admin_comments, [:namespace]
+    #add_index :active_admin_comments, [:namespace]
 
   end
 
