4f9ff2a79c9f2596718a41bf3fa6f45031c167a0
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-10 11:54:13 -0300

    modf models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :locale, presence: true
   include DeviseTokenAuth::Concerns::User
   has_many :rents, dependent: :nullify
   has_many :books, through: :rents
+  has_many :book_sugestions, dependent: :nullify
 end
