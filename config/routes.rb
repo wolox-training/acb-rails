d8e6e0d483f28c3a9438c943be307388d92c24c8
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-10 14:32:27 -0300

    BookSuggestionsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   api_version(module: 'Api::V1', path: { value: 'api/v1' }) do
-    resources :books, only: %i[index show]
-    resources :rents, only: %i[create index]
+    resources :books, only: [:index, :show]
+    resources :rents, only: [:create, :index]
     resources :book_suggestions, only: [:create]
   end
 end
