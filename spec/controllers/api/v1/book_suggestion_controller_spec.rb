cbd16ae606e2e00e59ce172ee6253921fa820fbf
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-11 13:30:48 -0300

    add post #create to book_suggestion_controller

--- spec/controllers/api/v1/book_suggestion_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestion_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
-
+require 'support/shared_contexts/authenticated_user.rb'
 describe Api::V1::BookSuggestionsController, type: :controller do
+  include_context 'Authenticated User'
+  describe 'POST #create' do
+    context 'When creating a valid book suggestion' do
+      let(:user) { create(:user)}
+      let!(:book_suggestion_attributes) { attributes_for(:book_suggestion)}
+      before do
+        request.headers.merge! user.create_new_auth_token
+        post :create, params: { book_suggestion: book_suggestion_attributes }
+      end
+      it 'creates a new book suggestion' do
+        request  = JSON.parse(response.body)
+        expect(request['user_id']).to eq user.id
+      end
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
 
+
+  
 end
--- spec/factories/rent.rb
+++ spec/factories/rent.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rent do
     from    { Faker::Date.today }
     to      { Faker::Date.forward(2) }
-    book    { 1 }
-    user    { 1 }
+    book    { create(:book) }
+    user    { create(:user) }
   end
 end
