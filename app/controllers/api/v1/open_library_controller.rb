1166f222fa69778d8b1125f05f251ce2a5079793
AGUSTIN BAJO <agustinbajo@gmail.com>
2018-10-18 11:22:48 -0300

    modif open_library_controller.rb / open_library_service.rb / routes.rb

--- app/controllers/api/v1/open_library_controller.rb
+++ app/controllers/api/v1/open_library_controller.rb
@@ -3,8 +3,15 @@
 module Api
   module V1
     class OpenLibraryController < ApiController
+      skip_before_action :authenticate_user!, only: [:show]
       def show
-        render json: OpenLibraryService.book_info(params[:id])
+        return render json: OpenLibraryService.new.book_info(params[:id])
+      rescue JSON::ParserError, TypeError => e
+        return 'JSON TypeError'
+      rescue NoMethodError
+        return "Book not found"
+      rescue SocketError
+        return 'Error 404'
       end
     end
   end
--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,26 +1,34 @@
 class OpenLibraryService
     include HTTParty
-  
-    base_uri 'https://openlibrary.org/api/books'
-    default_params format: 'json', jscmd: 'data'
-    format :json
-  
-    def self.book_info(isbn)
-      response = get(base_uri, query: { bibkeys: isbn })
-      if !response.nil?
-        cleanup(response.parsed_response[isbn], isbn)
-      else
-        message[:error] = 'No response from Open Library'
-      end
+    base_uri  'https://openlibrary.org'
+    
+    def book_info(isbn)
+      url_isbn = "ISBN:#{isbn}"
+      begin
+      parsed_response = findbook(url_isbn)
+      end 
+      build_hash(parsed_response, isbn, url_isbn)
+    end 
+
+    protected
+
+    def build_hash(api_res, isbn, url_isbn)
+      {
+        isbn: isbn,
+        title: api_res[url_isbn]['title'],
+        subtitle: api_res[url_isbn]['subtitle'],
+        number_of_pages: api_res[url_isbn]['number_of_pages'],
+        authors: api_res[url_isbn]['authors']
+      }
     end
-  
-    def self.cleanup(res, isbn)
-      json = {}
-      json[:ISBN] = isbn
-      json[:title] = res['title']
-      json[:subtitle] = res['subtitle']
-      json[:number_of_pages] = res['number_of_pages']
-      json[:authors] = res['authors']
-      json
+    def make_header
+      { 'Accept' => 'application/json' }
     end
+    def make_options(url_isbn)
+      { bibkeys: url_isbn, format: 'json', jscmd: 'data' }
+    end
+    def findbook(url_isbn)
+      response=self.class.get('/api/books', query: make_options(url_isbn), headers: make_header)
+      return  JSON.parse(response.body)
+    end 
 end
